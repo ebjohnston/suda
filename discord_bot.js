9a2e14ec39a59b1c23efc8c28de9e0675c9c6ede
Ethan Johnston <ej88572@gmail.com>
2017-04-11 22:11:30 -0600

    finalized warframe functionality

--- discord_bot.js
+++ discord_bot.js
@@ -6,9 +6,11 @@ const ArrayList = require("arraylist");
 
 // initialize imported modules
 const bot = new Discord.Client();
+
 var alerts_posted = new ArrayList;
 var invasions_posted = new ArrayList;
-//const warframeWorld = new WarframeWorldState(json-data);
+var news_posted = new ArrayList;
+var weekends_posted = new ArrayList;
 
 // configuration settings
 const settings = require("./settings.json");
@@ -79,7 +81,7 @@ var commands = {
     "baro": {
         name: "baro",
         description: "retrieve current info on Void Trader",
-        help: "scrapes current worldState.php from warframe and returns Void Trader information",
+        help: "scrapes current worldState.php from Warframe and returns Void Trader information",
         usage: "[No parameters]",
         admin: false,
         warframe: true,
@@ -90,12 +92,43 @@ var commands = {
                 }
                 else {
                     var warframe_world = new WarframeWorldState(body);
-                    msg.channel.sendMessage("`" + warframe_world.voidTrader.toString() + "`");
+                    msg.channel.sendMessage("```" + warframe_world.voidTrader.toString() + "```");
                 }
             });
 
             if (suffix) {
                 msg.channel.sendMessage("Note: baro takes no arguments");
+            }
+        }
+    },
+    "acolytes": {
+        name: "acolytes",
+        description: "retrieve current info on warframe acolytes",
+        help: "scrapes current worldState.php from Warframe and returns all active acolytes",
+        usage: "[No parameters]",
+        admin: false,
+        warframe: true,
+        process: (bot, msg, suffix) => {
+            Request('http://content.warframe.com/dynamic/worldState.php', (error, response, body) => {
+                if (response.statusCode != 200) {
+                    msg.channel.sendMessage("An error has occured: webpage not accessible");
+                }
+                else {
+                    var warframe_world = new WarframeWorldState(body);
+
+                    if (warframe_world.persistentEnemies.length > 0) {
+                        for (var i in warframe_world.persistentEnemies) {
+                            msg.channel.sendMessage("```" + warframe_world.persistentEnemies[i].toString() + "```");
+                        }
+                    }
+                    else {
+                        msg.channel.sendMessage("```There are currently no acolytes active in Warframe.```");
+                    }
+                }
+            });
+
+            if (suffix) {
+                msg.channel.sendMessage("Note: acolytes takes no arguments");
             }
         }
     }
@@ -140,7 +173,6 @@ bot.on("message", msg => {
 
     // acknowledge that a message was received
     console.log("message received: " + msg);
-    //Logger.log("info", msg.author.username + " executed <" + msg.content + ">");
 
     var command_text = msg.content.split(" ")[0].substring(1).toLowerCase();
     var suffix = msg.content.substring(command_text.length + 2); //add one for the ! and one for the space
@@ -153,7 +185,6 @@ bot.on("message", msg => {
 });
 
 function scrapeWarframe() {
-
     setInterval( () => {
         Request('http://content.warframe.com/dynamic/worldState.php', (error, response, body) => {
             if (response.statusCode != 200) {
@@ -164,14 +195,17 @@ function scrapeWarframe() {
                 var warframe_channel = bot.guilds.find("name", "Kvasir").channels.find("name", "warframe");
 
                 if (warframe_world && warframe_channel) {
-                    displayAlerts(warframe_world, warframe_channel);
+                    processAlerts(warframe_world, warframe_channel);
+                    processInvasions(warframe_world, warframe_channel);
+                    processNews(warframe_world, warframe_channel);
+                    processWeekends(warframe_world, warframe_channel);
                 }
             }
         });
     }, 60000);
 }
 
-function displayAlerts(warframe_world, warframe_channel) {
+function processAlerts(warframe_world, warframe_channel) {
     // buffer to cleanse list of stored alerts
     var current_alerts = new ArrayList;
 
@@ -189,7 +223,10 @@ function displayAlerts(warframe_world, warframe_channel) {
 
     // remove alerts once they expire
     alerts_posted = alerts_posted.intersection(current_alerts);
+}
 
+function processInvasions(warframe_world, warframe_channel) {
+    // buffer to cleanse the list of stored invasions
     var current_invasions = new ArrayList;
 
     for (var i in warframe_world.invasions) {
@@ -208,7 +245,49 @@ function displayAlerts(warframe_world, warframe_channel) {
     invasions_posted = invasions_posted.intersection(current_invasions);
 }
 
-//TODO: add custom item matching
+function processNews(warframe_world, warframe_channel) {
+    var today = new Date();
+    // buffer to cleanse list of stored news
+    var current_news = new ArrayList;
+
+    for (var i in warframe_world.news) {
+        news = warframe_world.news[i];
+
+        // omit duplicate news and old (preious days') news
+        if (!news_posted.contains(news.id) && today.getDate() === news.date.getDate()
+                && today.getMonth() === news.date.getMonth()) {
+            warframe_channel.sendMessage(news.link);
+            news_posted.add(news.id);
+        }
+
+        // populate news buffer
+        current_news.add(news.id);
+    }
+
+    // remove news once it expires
+    news_posted = news_posted.intersection(current_news);
+}
+
+function processWeekends(warframe_world, warframe_channel) {
+    // buffer to cleanse previous weekend events
+    var current_weekends = new ArrayList;
+
+    for (var i in warframe_world.globalUpgrades) {
+        weekend = warframe_world.globalUpgrades[i];
+
+        if(!weekends_posted.contains(weekend.upgrade)) {
+            warframe_channel.sendMessage("```" + weekend.toString() + "```");
+            weekends_posted.add(weekend.upgrade);
+        }
+
+        current_weekends.add(weekend.upgrade);
+    }
+
+    // remove weekend events once they expire
+    weekends_posted = weekends_posted.intersection(current_weekends);
+}
+
+// TODO: add custom item matching
 function hasGoodItem(event) {
     var item_regex = /nitain|forma|catalyst|reactor|exilus|kubrow|kavat|vandal|wraith/;
 
