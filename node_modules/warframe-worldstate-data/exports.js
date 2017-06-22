b9037b09391ca774ad0a4910471989ba8510030e
Ethan Johnston <ej88572@gmail.com>
2017-06-21 21:57:45 -0600

    added node modules temporarily

---
+++ node_modules/warframe-worldstate-data/exports.js
@@ -0,0 +1,47 @@
+const conclave = require('./data/conclaveData.json');
+const events = require('./data/eventsData.json');
+const factions = require('./data/factionsData.json');
+const fissureModifiers = require('./data/fissureModifiers.json');
+const languages  = require('./data/languages.json');
+const missionTypes = require('./data/missionTypes.json');
+const operationTypes = require('./data/operationTypes.json');
+const persistentEnemy = require('./data/persistentEnemyData.json');
+const solNodes = require('./data/solNodes.json');
+const sortie = require('./data/sortieData.json');
+const syndicates = require('./data/syndicatesData.json');
+const upgradeTypes = require('./data/upgradeTypes.json');
+
+const en_US = {
+  conclave,
+  events,
+  factions,
+  fissureModifiers,
+  languages,
+  missionTypes,
+  languages,
+  missionTypes,
+  operationTypes,
+  persistentEnemy,
+  solNodes,
+  sortie,
+  syndicates,
+  upgradeTypes
+}
+
+module.exports = {
+   en_US,
+   conclave,
+   events,
+   factions,
+   fissureModifiers,
+   languages,
+   missionTypes,
+   languages,
+   missionTypes,
+   operationTypes,
+   persistentEnemy,
+   solNodes,
+   sortie,
+   syndicates,
+   upgradeTypes
+}
